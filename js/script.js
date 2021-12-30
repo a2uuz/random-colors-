ac03ba7148c958651358792715679e4f1f2b462f
a2uuz <aziznageeye9@gmail.com>
2021-12-30 22:59:46 +0300

    added functions and comments

--- js/script.js
+++ js/script.js
@@ -1,14 +1,26 @@
 let html = '';
-let red;
-let green;
-let blue;
-let randomRGB;
- for (let i = 1; i <= 12; i++){
-red = Math.floor(Math.random() * 256);
-green = Math.floor(Math.random() * 256);
-blue = Math.floor(Math.random() * 256);
-randomRGB = `rgb( ${red}, ${green}, ${blue} )`;
-html += `<div style="background-color: ${randomRGB}">${i}</div>`;
+/**
+ * function that out puts a random number
+ * between 0 to 256 it's for the rgb numbers to be chosen 
+ * randomly 
+ * @returns used both Math floor and math random methods
+ *  for the calculation
+ */
+const randomValue = () => Math.floor(Math.random() * 256);
+/**
+ * this function holds the rgb colors to use 
+ * i called the value in the parameter which it's arguments
+ * is the randomValue which gives a random number
+ * for the 3 rgb colors to be picked 
+ * @returns 
+ */
+function RGB(value) {
+const color = `rgb(${value()}, ${value()}, ${value()})`;
+return color;
+}
 
+ for (let i = 1; i <= 12; i++){
+
+html += `<div style="background-color: ${RGB(randomValue)}">${i}</div>`;
  }
 document.querySelector('main').innerHTML = html;
